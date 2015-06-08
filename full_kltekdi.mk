c47b0b59c3dbb9e0a92cc631ed782179b161057b
Jay <kwangaho@gmail.com>
2015-06-08 20:08:53 +0800

    remove felica blobs for klte

--- full_kltekdi.mk
+++ full_kltekdi.mk
@@ -18,9 +18,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from kltekdi device
 $(call inherit-product, device/samsung/kltekdi/device.mk)
 
-# FeliCa vendor tree
-$(call inherit-product-if-exists, vendor/samsung/klte-felica-common/felica-common-vendor.mk)
-
 # Set those variables here to overwrite the inherited values.
 PRODUCT_NAME := full_kltekdi
 PRODUCT_DEVICE := kltekdi
